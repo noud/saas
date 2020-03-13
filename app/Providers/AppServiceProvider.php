fce7d8f46099b7a6d977c22fb7f958b18d42426e
Noud de Brouwer <noud4@home.nl>
2020-03-13 09:57:22 +0100

    generator

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if ($this->app->environment() == 'local') {
+            $this->app->register(\Reliese\Coders\CodersServiceProvider::class);
+        }
     }
 
     /**
