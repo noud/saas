f3e4aea2869ac3ead55029af298e26126f83927c
Noud de Brouwer <noud4@home.nl>
2020-03-14 04:59:14 +0100

    models user auth

--- app/User.php
+++ app/User.php
@@ -5,10 +5,26 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Joselfonseca\LighthouseGraphQLPassport\HasLoggedInTokens;
+use Joselfonseca\LighthouseGraphQLPassport\HasSocialLogin;
+use Joselfonseca\LighthouseGraphQLPassport\MustVerifyEmailGraphQL;
 
 class User extends Authenticatable
 {
+    // Authentication
+
+    // use HasApiTokens;
+    use HasSocialLogin;
+    use MustVerifyEmailGraphQL;
+    use HasLoggedInTokens;
     use Notifiable;
+	
+	public function withAccessToken()
+	{
+        return true;
+    }
+
+    // Attributes
 
     /**
      * The attributes that are mass assignable.
@@ -36,9 +52,4 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-	
-	public function withAccessToken()
-	{
-        return true;
-    }
 }
