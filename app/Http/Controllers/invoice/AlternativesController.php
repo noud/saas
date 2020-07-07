ffe43fc14abf2d09e7a1668443ab2eb54780758c
Noud de Brouwer <noud4@home.nl>
2020-07-07 11:50:16 +0200

    example alternatives works

--- app/Http/Controllers/invoice/AlternativesController.php
+++ app/Http/Controllers/invoice/AlternativesController.php
@@ -3,18 +3,18 @@
 namespace App\Http\Controllers\invoice;
 
 use App\Http\Controllers\Controller;
-use App\Http\Controllers\invoice;
-use Invoice;
+use LaravelDaily\Invoices\Invoice;
+// use Invoice;
 
 class AlternativesController extends Controller
 {
-     function alternatives() {
-   $customer = Invoice::makeParty([
+  function alternatives() {
+    $customer = Invoice::makeParty([
         'name' => 'John Doe',
     ]);
 
     $item = Invoice::makeItem('Your service or product title')->pricePerUnit(9.99);
 
     return Invoice::make()->buyer($customer)->addItem($item)->stream();
-    }
+  }
 }
